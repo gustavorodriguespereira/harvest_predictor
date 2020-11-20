f01270e6955bad6825dfe4a65db149bf5e175d50
Carlos Alisson Nunes <alisson.nunes@gmail.com>
2020-11-20 12:03:44 -0300

    model actually runs

--- predictor.py
+++ predictor.py
@@ -5,12 +5,13 @@ import numpy as np
 from scipy import stats
 import time
 import datetime
+from statistics import mean
 
 import matplotlib
 import matplotlib.pyplot as plt
 
 
-def harvest_predictor(raster, planted_date_as_str):
+def harvest_predictor(raster_url, planted_date_as_str):
     planted_date = datetime.datetime.strptime(planted_date_as_str, "%Y-%m-%d").date()
     # Model
     df = pd.read_csv("data/ndvi-curve.csv")
@@ -22,16 +23,19 @@ def harvest_predictor(raster, planted_date_as_str):
     target = np.polyval(model, days_since_planted)
 
     # Reading the NDVI from Leaf's
-    leaf_raster = rasterio.open(raster)
+    leaf_raster = rasterio.open(raster_url)
     ndvi = leaf_raster.read(1).astype('float64')
-    ndvi_list = [[x for x in y if not np.isnan(x)] for y in ndvi]
+    ndvi_list = []
+    for sublist in [[x for x in y if not np.isnan(x)] for y in ndvi]:
+        for item in sublist:
+            ndvi_list.append(item)
 
     ndvi = mean(ndvi_list)
 
     # Finding the date of the Leaf NDVI
 
     for pred in list(range(1, 247)):
-        actual = np.polyval(mymodel, pred)
+        actual = np.polyval(model, pred)
         if actual >= ndvi * 0.8 or actual <= ndvi * 1.2:
             break
 
